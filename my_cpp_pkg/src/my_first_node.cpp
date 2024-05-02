6190f201b6c69b7713204a3a40815b04ba58a838
Hari Karumuri <harikarumuri1086@gmail.com>
2024-05-02 12:21:15 +0530

    added counter to the private and initaialed to my node and spin loop

--- my_cpp_pkg/src/my_first_node.cpp
+++ my_cpp_pkg/src/my_first_node.cpp
@@ -3,11 +3,20 @@
 class MyNode : public rclcpp::Node{
     public:
         //constructor and also initialize at the same time
-        MyNode():Node("cpp_test"){
+        MyNode():Node("cpp_test"),counter_(0){
             RCLCPP_INFO(this->get_logger(),"Hello Cpp Node");
+            timer_ = this->create_wall_timer(std::chrono::seconds(1),
+            std::bind(&MyNode::timerCallback,this));
         }
 
     private:
+    void timerCallback(){
+        counter_++;
+        RCLCPP_INFO(this->get_logger(),"Hello %d",counter_);
+    }
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    int counter_;
 
 };
 
